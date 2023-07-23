7f2deda683b578e7d8b3c414e791a271d5b2f691
Tugulduroch <tkmusique.24@gmail.com>
2023-07-23 20:44:41 +0800

    fixed fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,7 @@ export const metadata: Metadata = {
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }
-  ],
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
-  }
+  ]
 }
 
 interface RootLayoutProps {
