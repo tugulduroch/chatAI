7d1e4ea3117d8a00c966ced98a1d1960c4b760cc
Tugulduroch <tkmusique.24@gmail.com>
2023-07-23 20:36:08 +0800

    paragraph fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,13 @@ import { ExternalLink } from '@/components/external-link'
 
 export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
   return (
-    <p>
+    <p
+      className={cn(
+        'px-2 text-center text-xs leading-normal text-muted-foreground',
+        className
+      )}
+      {...props}
+    >
       Developed by : Tugulduroch
     </p>
   )
